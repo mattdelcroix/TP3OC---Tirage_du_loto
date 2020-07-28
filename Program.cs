a62059bfcb2dfef3e2cd02a3ce60fad91586a8a8
Matthieu Delcroix <matt.delcroix@gmail.com>
2020-07-28 17:23:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace TP3OC_Tirage_du_loto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+
+            Random aleatoire = new Random();
+            int entier;
+
+            for (int i = 0; i < 7; i++)
+            {
+                do
+                {
+                  entier = aleatoire.Next(49) + 1;
+                } while (tab.Contains(entier));
+
+                tab[i] = entier;
+            }
+            Array.Sort(tab);
+
+            static void afficherTab(int[] tableau)
+            {
+                foreach(int elem in tableau)
+                {
+                    Console.WriteLine(elem);
+                }
+            }
+
+            afficherTab(tab);
+        }
+    }
+}
